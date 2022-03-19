568abcea11d31961c427ac9d0e94c4cf38ddc5be
FilipeDias23 <filipedias9423@gmail.com>
2022-03-19 17:14:04 +0000

    exercises done

---
+++ top10Movie.js
@@ -0,0 +1,6 @@
+const top10Movies = ["The Shawshank Redemption", "The Godfather", " The Dark Knight", "The GodfatherII", "12 Angry Men", "The Schindler List", "The Lord of the Rings: The Return of the King","Pulp Fiction", "The Lord of the Rings: The Fellowship of the Ring", "Fight Club"]
+
+for (var i=0; i < top10Movies.length; i++){
+  moviesList = top10Movies.forEach;
+  console.log(top10Movies[i])
+}

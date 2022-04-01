49875b7d32ebe46e4919eff75b779827e7bc321a
FilipeDias23 <filipediasit@gmail.com>
2022-04-01 20:22:51 +0100

    code prettier

--- age.js
+++ age.js
@@ -1,21 +1,15 @@
 const votingAge = function (age) {
-   
-    if (age >= 18 ) {
-      console.log("You have the age to vote")
-    }
-    else {
-      console.log("You dont have the age to vote")
-    }
-    ;
-    if (age < 21 ) {
-      return "You cant be elected as president"
-    }
-    else {
-      return "You can be elected as President"
-    }
-    
+  if (age >= 18) {
+    console.log("You have the age to vote");
+  } else {
+    console.log("You dont have the age to vote");
   }
-  
-  console.log(votingAge(18))
-  
-  // Porque é que me aparece undefined ? //
+  if (age < 21) {
+    return "You cant be elected as president";
+  } else {
+    return "You can be elected as President";
+  }
+};
+
+console.log(votingAge(18));
+
